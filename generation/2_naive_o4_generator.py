a9aceda29dadb5b0081c5c3a1166227418c208e9
arjun-banerjee <abaner@berkeley.edu>
2025-03-05 01:11:28 -0800

    Added prompting folder and a 4o pipeline generator, along with matrix multiplication kernel it generated

---
+++ generation/2_naive_o4_generator.py
@@ -0,0 +1,42 @@
+from openai import OpenAI
+
+def get_chat_completion(system_prompt, user_prompt, model = "gpt-4o-mini", temperature = 0.7):
+    """
+    Returns the completion for the given system prompt and user prompt.
+    
+    Args:
+        system_prompt (str): The system prompt.
+        user_prompt (str): The user prompt.
+        model (str): The model to use. Defaults to "gpt-4o-mini".
+        temperature (float): The temperature to use. Defaults to 0.7.
+    
+    Returns:
+        str: The completion.
+    """
+    #Set up the client
+    client = OpenAI()
+    #Perform the completion
+    completion = client.chat.completions.create(
+        model=model,
+        messages=[
+            #System prompt
+            {"role": "system", "content": system_prompt},
+            #User prompt
+            {"role": "user",   "content": user_prompt},
+        ],
+        temperature=temperature
+    )
+    #Return the content generated
+    return completion.choices[0].message.content
+
+
+if __name__ == "__main__":
+    #Access system and user prompts from text files in the prompts folder
+    with open("../prompts/system_prompt_naive.txt", "r") as f:
+        system_prompt = f.read()
+    with open("../prompts/user_prompt_naive.txt", "r") as f:
+        user_prompt = f.read()
+    result = get_chat_completion(system_prompt, user_prompt)
+    #save result as a txt file
+    with open("../generation/samples/2_naive_o4_generator.txt", "w") as f:
+        f.write(result)

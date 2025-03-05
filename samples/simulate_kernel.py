2fbb05788892f7ba56a3534bc2ee0acd38a9e868
Hoponga <kailash.ranganathan@gmail.com>
2025-03-04 22:55:02 -0800

    matmul works

--- samples/simulate_kernel.py
+++ samples/simulate_kernel.py
@@ -1,6 +1,8 @@
 import neuronxcc.nki as nki
 import neuronxcc.nki.language as nl
 import numpy as np
+from matrix_multiplication_nki_kernels import nki_matmul_basic_, nki_matmul_tiled_, nki_matmul_hoist_load_, nki_matmul_block_free_dimension_, nki_matmul_fully_optimized_
+
 
 
 @nki.jit
@@ -20,6 +22,24 @@ def print_kernel():
 
 np.random.seed(0)
 
-a = nki.simulate_kernel(print_kernel)
+import torch
 
-assert np.allclose(a, np.zeros([4, 4]))
+lhs_small = torch.rand((64, 128))
+rhs_small = torch.rand((128, 512))
+
+output_small = nki.simulate_kernel(nki_matmul_basic_, np.array(lhs_small.T), np.array(rhs_small))
+
+
+# Run torch reference
+output_small_torch = torch.matmul(lhs_small, rhs_small)
+
+  # Compare results
+print("Checking correctness of nki_matmul_basic")
+if torch.allclose(output_small_torch, torch.Tensor(output_small), atol=1e-4, rtol=1e-2):
+   print("NKI and Torch match")
+else:
+   print("NKI and Torch differ")
+
+
+
+
